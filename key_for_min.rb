8da81102558de36159d14d3d6bfe025f4cdb4967
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-21 16:55:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,4 +4,18 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     nil
   end
-end
+name_a = name_hash.to_a
+  
+  d_value= 1000
+  d_key= 0
+
+  name_a.each do |i|
+      if i[1] < d_value
+        d_value = i[1]
+        d_key = i[0]
+      end
+
+   end
+d_key
+
+end  
