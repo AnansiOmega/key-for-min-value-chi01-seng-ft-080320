96a770cc807a1800219770012b2bd207918628be
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-21 16:34:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,12 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-require 'pry'
 def key_for_min_value(name_hash)
- 
-name_hash.to_a
 array = name_hash.to_a
 smallest = 1000
-binding.pry
+
 array.each do |num|
   if num[0][1] < smallest
     smallest = num
