f8ee3cb617ea3ecbc47a6233173db3da6d228ec9
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-21 15:50:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,22 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+name_hash.
+end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
 name_hash.inject do |key,value|
  smallest = 1000
 if  value[1] < smallest
