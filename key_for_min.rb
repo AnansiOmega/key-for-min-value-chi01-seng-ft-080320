39adabfe3c508948da3c09b14ed5887fbb0687d1
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-21 16:53:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(name_hash)
   if name_hash == {}
-    NIL
+    nil
   end
   
   name_a = name_hash.to_a
