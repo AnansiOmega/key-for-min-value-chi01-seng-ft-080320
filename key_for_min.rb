264c2fd09b1d82e9e2ce41c38569a2af32337596
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-21 15:25:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@
 
 def key_for_min_value(name_hash)
 name_hash.each do |key,value|
-  smallest = 1000
   
-    value < smallest
+  
+  smallest = 1000
+if  value < smallest
     key = smallest
     end
-    key
 end
